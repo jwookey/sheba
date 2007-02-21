3d9934832518514410124a7e11c58bc9297628c0
wookey <wookey>
2007-02-21 13:23:06 +0000

    Updated for compatiblity with big-endian SAC data on little-endian machines (as, for example, used by MacSAC on intel-based Macs)

--- particle.f90
+++ particle.f90
@@ -7,7 +7,7 @@
 !===============================================================================
 !
 !  James Wookey, School of Earth Sciences, University of Bristol
-!  CVS: $Revision: 1.2 $ $Date: 2007/02/06 14:03:49 $
+!  CVS: $Revision: 1.3 $ $Date: 2007/02/21 13:23:06 $
 !
 
 !=======================================================================
@@ -25,11 +25,20 @@
       type (SACTrace) :: t1,t2 ! the traces
 
       integer :: lu
+      
+      integer, parameter :: int4 = selected_int_kind(9) ;
+      integer, parameter :: real4 = selected_real_kind(6,37) ;
+      
+      
       character (len=4) :: tmp_char1,tmp_char2,tmp_char3,tmp_char4
       character (len=50):: fname
       integer :: i, istatus
       type (SACtrace) :: out
       logical UNITOK, UNITOP
+
+      real(real4) :: sacrh(70) ! SAC floating point header
+      integer(int4) :: sacih(40) ! SAC floating point header
+      
 !     * Open the file for writing *
 !  ** Open the file for writing
       open (unit=f90sac_iounit, file=fname,form='unformatted', &
@@ -45,118 +54,132 @@
       t1 % yminimum = minval(t2%trace(1:t1 % npts))
 
 !
-!     * READ IN THE SAC HEADER
+!     * WRITE OUT THE SAC HEADER
 !
-      write(lu,rec=001) t1%delta              
-      write(lu,rec=002) t1%depmin
-      write(lu,rec=003) t1%depmax
-      write(lu,rec=004) t1%scale
-      write(lu,rec=005) t1%odelta
-      write(lu,rec=006) t1%b
-      write(lu,rec=007) t1%e
-      write(lu,rec=008) t1%o
-      write(lu,rec=009) t1%a
-      write(lu,rec=010) t1%internal0
-      write(lu,rec=011) t1%t0
-      write(lu,rec=012) t1%t1
-      write(lu,rec=013) t1%t2
-      write(lu,rec=014) t1%t3
-      write(lu,rec=015) t1%t4
-      write(lu,rec=016) t1%t5
-      write(lu,rec=017) t1%t6
-      write(lu,rec=018) t1%t7
-      write(lu,rec=019) t1%t8
-      write(lu,rec=020) t1%t9
-      write(lu,rec=021) t1%f
-      write(lu,rec=022) t1%resp0
-      write(lu,rec=023) t1%resp1
-      write(lu,rec=024) t1%resp2
-      write(lu,rec=025) t1%resp3
-      write(lu,rec=026) t1%resp4
-      write(lu,rec=027) t1%resp5
-      write(lu,rec=028) t1%resp6
-      write(lu,rec=029) t1%resp7
-      write(lu,rec=030) t1%resp8
-      write(lu,rec=031) t1%resp9
-      write(lu,rec=032) t1%stla
-      write(lu,rec=033) t1%stlo
-      write(lu,rec=034) t1%stel
-      write(lu,rec=035) t1%stdp
-      write(lu,rec=036) t1%evla
-      write(lu,rec=037) t1%evlo
-      write(lu,rec=038) t1%evel
-      write(lu,rec=039) t1%evdp
-      write(lu,rec=040) t1%mag
-      write(lu,rec=041) t1%user0
-      write(lu,rec=042) t1%user1
-      write(lu,rec=043) t1%user2
-      write(lu,rec=044) t1%user3
-      write(lu,rec=045) t1%user4
-      write(lu,rec=046) t1%user5
-      write(lu,rec=047) t1%user6
-      write(lu,rec=048) t1%user7
-      write(lu,rec=049) t1%user8
-      write(lu,rec=050) t1%user9
-      write(lu,rec=051) t1%dist
-      write(lu,rec=052) t1%az
-      write(lu,rec=053) t1%baz
-      write(lu,rec=054) t1%gcarc
-      write(lu,rec=055) t1%internal1
-      write(lu,rec=056) t1%internal2
-      write(lu,rec=057) t1%depmen
-      write(lu,rec=058) t1%cmpaz
-      write(lu,rec=059) t1%cmpinc
-      write(lu,rec=060) t1%xminimum
-      write(lu,rec=061) t1%xmaximum
-      write(lu,rec=062) t1%yminimum
-      write(lu,rec=063) t1%ymaximum
-      write(lu,rec=064) t1%unused1
-      write(lu,rec=065) t1%unused2
-      write(lu,rec=066) t1%unused3
-      write(lu,rec=067) t1%unused4
-      write(lu,rec=068) t1%unused5
-      write(lu,rec=069) t1%unused6
-      write(lu,rec=070) t1%unused7
-      write(lu,rec=071) t1%nzyear
-      write(lu,rec=072) t1%nzjday
-      write(lu,rec=073) t1%nzhour
-      write(lu,rec=074) t1%nzmin
-      write(lu,rec=075) t1%nzsec
-      write(lu,rec=076) t1%nzmsec
-      write(lu,rec=077) t1%nvhdr
-      write(lu,rec=078) t1%norid
-      write(lu,rec=079) t1%nevid
-      write(lu,rec=080) t1%npts
-      write(lu,rec=081) t1%internal3
-      write(lu,rec=082) t1%nwfid
-      write(lu,rec=083) t1%nxsize
-      write(lu,rec=084) t1%nysize
-      write(lu,rec=085) t1%unused8
-      write(lu,rec=086) 4
-      write(lu,rec=087) t1%idep
-      write(lu,rec=088) t1%iztype
-      write(lu,rec=089) t1%unused9
-      write(lu,rec=090) t1%iinst
-      write(lu,rec=091) t1%istreg
-      write(lu,rec=092) t1%ievreg
-      write(lu,rec=093) t1%ievtyp
-      write(lu,rec=094) t1%iqual
-      write(lu,rec=095) t1%isynth
-      write(lu,rec=096) t1%imagtyp
-      write(lu,rec=097) t1%imagsrc
-      write(lu,rec=098) t1%unused10
-      write(lu,rec=099) t1%unused11
-      write(lu,rec=100) t1%unused12
-      write(lu,rec=101) t1%unused13
-      write(lu,rec=102) t1%unused14
-      write(lu,rec=103) t1%unused15
-      write(lu,rec=104) t1%unused16
-      write(lu,rec=105) t1%unused17
-      write(lu,rec=106) 0!! BECAUSE A XY FILE NOW
-      write(lu,rec=107) t1%lpspol
-      write(lu,rec=108) t1%lovrok
-      write(lu,rec=109) t1%lcalda
-      write(lu,rec=110) t1%unused18
+      sacrh(001)  = t1%delta              
+      sacrh(002)  = t1%depmin
+      sacrh(003)  = t1%depmax
+      sacrh(004)  = t1%scale
+      sacrh(005)  = t1%odelta
+      sacrh(006)  = t1%b
+      sacrh(007)  = t1%e
+      sacrh(008)  = t1%o
+      sacrh(009)  = t1%a
+      sacrh(010)  = t1%internal0
+      sacrh(011)  = t1%t0
+      sacrh(012)  = t1%t1
+      sacrh(013)  = t1%t2
+      sacrh(014)  = t1%t3
+      sacrh(015)  = t1%t4
+      sacrh(016)  = t1%t5
+      sacrh(017)  = t1%t6
+      sacrh(018)  = t1%t7
+      sacrh(019)  = t1%t8
+      sacrh(020)  = t1%t9
+      sacrh(021)  = t1%f
+      sacrh(022)  = t1%resp0
+      sacrh(023)  = t1%resp1
+      sacrh(024)  = t1%resp2
+      sacrh(025)  = t1%resp3
+      sacrh(026)  = t1%resp4
+      sacrh(027)  = t1%resp5
+      sacrh(028)  = t1%resp6
+      sacrh(029)  = t1%resp7
+      sacrh(030)  = t1%resp8
+      sacrh(031)  = t1%resp9
+      sacrh(032)  = t1%stla
+      sacrh(033)  = t1%stlo
+      sacrh(034)  = t1%stel
+      sacrh(035)  = t1%stdp
+      sacrh(036)  = t1%evla
+      sacrh(037)  = t1%evlo
+      sacrh(038)  = t1%evel
+      sacrh(039)  = t1%evdp
+      sacrh(040)  = t1%mag
+      sacrh(041)  = t1%user0
+      sacrh(042)  = t1%user1
+      sacrh(043)  = t1%user2
+      sacrh(044)  = t1%user3
+      sacrh(045)  = t1%user4
+      sacrh(046)  = t1%user5
+      sacrh(047)  = t1%user6
+      sacrh(048)  = t1%user7
+      sacrh(049)  = t1%user8
+      sacrh(050)  = t1%user9
+      sacrh(051)  = t1%dist
+      sacrh(052)  = t1%az
+      sacrh(053)  = t1%baz
+      sacrh(054)  = t1%gcarc
+      sacrh(055)  = t1%internal1
+      sacrh(056)  = t1%internal2
+      sacrh(057)  = t1%depmen
+      sacrh(058)  = t1%cmpaz
+      sacrh(059)  = t1%cmpinc
+      sacrh(060)  = t1%xminimum
+      sacrh(061)  = t1%xmaximum
+      sacrh(062)  = t1%yminimum
+      sacrh(063)  = t1%ymaximum
+      sacrh(064)  = t1%unused1
+      sacrh(065)  = t1%unused2
+      sacrh(066)  = t1%unused3
+      sacrh(067)  = t1%unused4
+      sacrh(068)  = t1%unused5
+      sacrh(069)  = t1%unused6
+      sacrh(070)  = t1%unused7
+
+      if (f90sac_force_byteswap) then
+         call f90sac_real32_byteswap(sacrh,70)
+      endif
+
+      sacih(001) =t1%nzyear
+      sacih(002) =t1%nzjday
+      sacih(003) =t1%nzhour
+      sacih(004) =t1%nzmin
+      sacih(005) =t1%nzsec
+      sacih(006) =t1%nzmsec
+      sacih(007) =t1%nvhdr
+      sacih(008) =t1%norid
+      sacih(009) =t1%nevid
+      sacih(010) =t1%npts
+      sacih(011) =t1%internal3
+      sacih(012) =t1%nwfid
+      sacih(013) =t1%nxsize
+      sacih(014) =t1%nysize
+      sacih(015) =t1%unused8
+      sacih(016) =4
+      sacih(017) =t1%idep
+      sacih(018) =t1%iztype
+      sacih(019) =t1%unused9
+      sacih(020) =t1%iinst
+      sacih(021) =t1%istreg
+      sacih(022) =t1%ievreg
+      sacih(023) =t1%ievtyp
+      sacih(024) =t1%iqual
+      sacih(025) =t1%isynth
+      sacih(026) =t1%imagtyp
+      sacih(027) =t1%imagsrc
+      sacih(028) =t1%unused10
+      sacih(029) =t1%unused11
+      sacih(030) =t1%unused12
+      sacih(031) =t1%unused13
+      sacih(032) =t1%unused14
+      sacih(033) =t1%unused15
+      sacih(034) =t1%unused16
+      sacih(035) =t1%unused17
+      sacih(036) =0!! BECAUSE A XY FILE NOW
+      sacih(037) =t1%lpspol
+      sacih(038) =t1%lovrok
+      sacih(039) =t1%lcalda
+      sacih(040) =t1%unused18
+
+      if (f90sac_force_byteswap) then
+         call f90sac_int32_byteswap(sacih,40)
+      endif
+
+      do i=1,40
+         write(lu,rec=i+70) sacih(i)
+      enddo
+
       
       write(lu,rec=111) t1%kstnm(1:4)
       write(lu,rec=112) t1%kstnm(5:8)
@@ -207,19 +230,28 @@
       write(lu,rec=157) t1%kinst(1:4)
       write(lu,rec=158) t1%kinst(5:8)
 
-!     * Output the first trace *   
+
+!  ** if required, byteswap the first trace
+      if (f90sac_force_byteswap) then
+         call f90sac_real32_byteswap(t1%trace,t1%npts)
+      endif
+
+!  ** Output the first trace   
        do i=1,t1 % npts
             write(lu,rec=158+i) t1%trace(i)
       enddo
-      
-!      print*,maxval(t1%trace(1:t1 % npts))
-      
-!     * Output the first trace *   
+
+!  ** if required, byteswap the first trace
+      if (f90sac_force_byteswap) then
+         call f90sac_real32_byteswap(t2%trace,t2%npts)
+      endif
+
+!  ** Output the first trace   
        do i=1,t2 % npts
-            write(lu,rec=158+i+t2 % npts) t2%trace(i)
+            write(lu,rec=158+i) t2%trace(i)
       enddo
 
- 
+
       close(lu)
       
 
