a8be88a0eb7ee1fae49eb47fa05cba95e06318db
James Wookey <j.wookey@bristol.ac.uk>
2021-08-20 07:01:25 +0100

    Added more fields to include output information (required for netCDF output)

--- event_info.f90
+++ event_info.f90
@@ -32,7 +32,33 @@
                                   ! projection                    
 !        ** other information
             real :: error_grid_tlag_int           
-            real*4 :: lam1_raw(np1,np2int),lam2_raw(np1,np2int) 
+
+!        ** parameter search space
+            real*4 :: fast_vector(np1), tlag_vector(np2int) 
+
+!        ** lam1, lam2 grids before normalisation
+            real*4 :: lam1_raw_grid(np1,np2int),lam2_raw_grid(np1,np2int) 
+
+!        ** error grids after f-test normalisation
+            real*4 :: lam2_norm_grid(np1,np2int)
+
+!        ** multiwindow information
+            integer :: nwindows
+            real*4 :: mw_wbeg(npc)
+            real*4 :: mw_wend(npc)
+            real*4 :: mw_tlag(npc)
+            real*4 :: mw_dtlag(npc)
+            real*4 :: mw_fast(npc)
+            real*4 :: mw_dfast(npc)
+
+!        ** cluster information
+            integer :: ncluster
+            integer :: kbest
+            real*4  :: cluster_xc0(npc)
+            real*4  :: cluster_yc0(npc)
+            real*4  :: cluster_vxc0(npc)
+            real*4  :: cluster_vyc0(npc)
+
          end type event_info_type     
 
          type (event_info_type),public :: event
